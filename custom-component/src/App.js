a48029aa92e13e09a3552a7c5407fb53b0c63955
Jebbit-koi <75734684+Jebbit-koi@users.noreply.github.com>
2022-02-07 09:50:05 +0900

    [Style] App

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,7 +1,42 @@
 import "./App.css";
+import styled from "styled-components";
+import Toggle from "./component/Toggle";
+import Modal from "./component/Modal";
+import Tab from "./component/Tab";
+import Tag from "./component/Tag";
+import AutoComplete from "./component/AutoComplete";
+import ClickToEdit from "./component/ClickToEdit";
+
+const Container = styled.div`
+  height: 250px;
+  border: 1px solid #ccc;
+  margin: 20px;
+  border-radius: 20px;
+`;
 
 function App() {
-  return <div className="App">wanted pre onboarding</div>;
+  return (
+    <>
+      <Container>
+        <Toggle />
+      </Container>
+      <Container>
+        <Modal />
+      </Container>
+      <Container>
+        <Tab />
+      </Container>
+      <Container>
+        <Tag />
+      </Container>
+      <Container>
+        <AutoComplete />
+      </Container>
+      <Container>
+        <ClickToEdit />
+      </Container>
+    </>
+  );
 }
 
 export default App;
